10a4f04c18aeef5a1e293e2142a289ff4ef5b6ab
Iury Vasconcelos <iury0393@gmail.com>
2020-07-27 19:19:43 -0300

    Adicionando a intro screen

---
+++ mobile/lib/screens/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String id = '/login';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello World!'),
+    );
+  }
+}
