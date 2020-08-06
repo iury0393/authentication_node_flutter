a0642f3581ff22cc84b5b6ace6d037eda295d3d6
Iury Vasconcelos <iury0393@gmail.com>
2020-08-05 21:49:08 -0300

    Integrando login

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -140,6 +140,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         child: ActionButton(
                           btnIcon: FeatherIcons.logIn,
                           btnFnc: () {
+                            //Integrando login
                             signInUser();
                           },
                         ),
